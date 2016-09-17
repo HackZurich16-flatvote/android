40488714446097f48f63f3e0bbb0816f1550b005
Christof Buechi <christof.buechi@gmail.com>
2016-09-17 14:02:13 +0200

    add launcher icons

--- app/src/main/java/com/hackzurich/flatvote/flatvote/base/BaseApplication.java
+++ app/src/main/java/com/hackzurich/flatvote/flatvote/base/BaseApplication.java
@@ -1,19 +1,19 @@
 package com.hackzurich.flatvote.flatvote.base;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.hackzurich.flatvote.flatvote.utils.dagger.component.AppComponent;
 
 /**
  * Created by christof on 17.09.16.
  */
-public class BaseApplication extends Application{
+public class BaseApplication extends MultiDexApplication {
 
 
     @Override
     public void onCreate() {
         super.onCreate();
         AppComponent.Holder.initialize(this);
-
     }
 }
