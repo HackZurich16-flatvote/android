aef8be37c788df8088c2140042bf242b8be74c28
Christof Buechi <christof.buechi@gmail.com>
2016-09-17 14:04:04 +0200

    rename files

--- app/src/main/java/com/hackzurich/flatvote/flatvote/LoginFragment.java
+++ app/src/main/java/com/hackzurich/flatvote/flatvote/LoginFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,17 +15,16 @@ import butterknife.ButterKnife;
 /**
  * Created by christof on 17.09.16.
  */
-public class selectFragment extends Fragment {
+public class LoginFragment extends Fragment {
 
+    @BindView(R.id.btn_login)
+    protected AppCompatButton button1;
 
-
-    @BindView(R.id.btn_search)
-    AppCompatButton button1;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.selectfragment, container, false);
+        View view = inflater.inflate(R.layout.loginfragment, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
@@ -34,12 +34,13 @@ public class selectFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        button1.setOnClickListener(createClickListener());
+            button1.setOnClickListener(createClickListener());
     }
 
     private View.OnClickListener createClickListener() {
         return v -> {
-            // TODO: 17.09.16 do search
+            // TODO: 17.09.16 do login
+            Toast.makeText(LoginFragment.this.getActivity(),"fuuuubar",Toast.LENGTH_LONG).show();
         };
     }
 }
--- app/src/main/java/com/hackzurich/flatvote/flatvote/SelectFragment.java
+++ app/src/main/java/com/hackzurich/flatvote/flatvote/SelectFragment.java
@@ -7,8 +7,6 @@ import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,16 +14,17 @@ import butterknife.ButterKnife;
 /**
  * Created by christof on 17.09.16.
  */
-public class loginFragment extends Fragment {
+public class SelectFragment extends Fragment {
 
-    @BindView(R.id.btn_login)
-    protected AppCompatButton button1;
 
+
+    @BindView(R.id.btn_search)
+    AppCompatButton button1;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.loginfragment, container, false);
+        View view = inflater.inflate(R.layout.selectfragment, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
@@ -35,13 +34,12 @@ public class loginFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-            button1.setOnClickListener(createClickListener());
+        button1.setOnClickListener(createClickListener());
     }
 
     private View.OnClickListener createClickListener() {
         return v -> {
-            // TODO: 17.09.16 do login
-            Toast.makeText(loginFragment.this.getActivity(),"fuuuubar",Toast.LENGTH_LONG).show();
+            // TODO: 17.09.16 do search
         };
     }
 }
